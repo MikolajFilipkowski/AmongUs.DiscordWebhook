b1e18d5a493363682b8ff7aea7d2f9d3c5b5aa99
Mikolaj Filipkowski <mikolajfilipkowski000@gmail.com>
2023-06-29 17:20:00 +0200

    First commit

---
+++ DiscordWebhook/DiscordWebhook.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Reactor;
+using Reactor.Utilities;
+
+namespace DiscordWebhook;
+
+[BepInAutoPlugin("com.vexi.discordwebhook", "Discord Webhook", VersionString)]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+public partial class DiscordWebhookPlugin : BasePlugin
+{
+    public const string VersionString = "1.0.0";
+    public static ConfigEntry<string> WebhookUrl { get; set; }
+
+    public Harmony Harmony { get; } = new("com.vexi.discordwebhook");
+
+    
+
+    public override void Load()
+    {
+        Harmony.PatchAll();
+        WebhookUrl = Config.Bind("Custom", "WebhookUrl", "");
+    }
+}
